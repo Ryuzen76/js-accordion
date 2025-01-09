c93504b7fd97b446fd27cd9fc5540ef0ce8915a5
Fahim <fahimardani25@gmail.com>
2025-01-09 13:14:22 +0700

    ADD - Accordion

---
+++ script.js
@@ -0,0 +1,21 @@
+const accordionHeader = document.querySelectorAll('.accordion-header')
+
+accordionHeader.forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+        const arrow = button.querySelector('.arrow');
+        
+        accordionContent.classList.toggle('active');
+        arrow.classList.toggle('active');
+
+        const bgImage = button.getAttribute('data-bg');
+        document.body.style.backgroundImage = bgImage;
+        
+        document.querySelectorAll('.accordion-content').forEach(content => {
+            if (content !== accordionContent) {
+                content.classList.remove('active');
+                content.previousElementSibling.querySelector('.arrow').classList.remove('active');
+            }
+        });
+    });
+});
